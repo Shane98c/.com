4f1f312a80a304faac07d45fc9f42a67c063739f
Shane98c <shane98c@GMAIL.COM>
2018-08-27 18:16:58 -0500

    duties

--- components/Duties.js
+++ components/Duties.js
@@ -1,9 +1,16 @@
-var Duties = props => (
+const getDuties = duties => duties.map(duty => <span>{duty}</span>);
+
+const Duties = props => (
   <div>
-    {props.duties.map(duty => (
-      <span>{duty}</span>
-    ))}
+    <div className="duty">{getDuties(props.duties)}</div>
+    <style jsx>{`
+      .duty {
+        display: flex;
+        justify-content: space-around;
+        flex-flow: row wrap;
+        margin: 10px;
+      }
+    `}</style>
   </div>
 );
-
 export default Duties;

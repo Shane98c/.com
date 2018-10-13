ca1cc9a6fbb2e3bd933bd7d58752c4a545de7a48
Shane98c <shane98c@GMAIL.COM>
2018-10-12 19:14:50 -0500

    writing

--- components/Post.js
+++ components/Post.js
@@ -2,15 +2,25 @@ import styled from "styled-components";
 const Wrapper = styled.div`
   .post {
     padding: 20px 20px 5px 20px;
-
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(500px, 1fr));
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+  }
+  .text {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    width: 50%;
+    min-width: 300px;
   }
   .title {
+    font-size: 180%;
     font-weight: bold;
+    color: ${({ color }) => color || "black"};
   }
   .heroImg {
-    width: 100%;
+    max-width: 50%;
+    min-width: 300px;
   }
   .heroImg img {
     width: 100%;
@@ -20,7 +30,15 @@ const Wrapper = styled.div`
 const Post = props => (
   <Wrapper>
     <div className="post">
-      <div className="title">{props.title}</div>
+      <div className="text">
+        <div className="title">
+          <a href={props.link} target="_blank">
+            {props.title}
+          </a>
+        </div>
+        <div className="snip">{props.snippet}</div>
+      </div>
+
       <div className="heroImg">
         <img src={props.img} />
       </div>
--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ const projects = [
   {
     img: "../static/img/mgv.png",
     title: "MultiGeoVis",
-    sub: "Visualizing multivariate spatiotemporal data on mobile devices",
+    sub: "Visualizing multivariate spatiotemporal data on mobile devices.",
     linkName: "github.com/Shane98c/multigeovis",
     link: "https://github.com/Shane98c/multigeovis",
     duties: ["concept", "design", "development"],

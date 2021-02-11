03093d907d7bfd200fc8703a15d1ea470fac3709
Shane98c <shane98c@GMAIL.COM>
2021-02-11 17:38:27 -0600

    fix FOUC

--- pages/_document.js
+++ pages/_document.js
@@ -2,39 +2,29 @@ import Document, { Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
-    const page = renderPage((App) => (props) =>
-      sheet.collectStyles(<App {...props} />)
-    );
-    const styleTags = sheet.getStyleElement();
-    return { ...page, styleTags };
-  }
+    const originalRenderPage = ctx.renderPage;
 
-  render() {
-    return (
-      <html>
-        <Head>
-          {
-            <style
-              dangerouslySetInnerHTML={{
-                __html: `
-body #__next div {visibility: hidden}
-body.loaded #__next div {visibility: visible}
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
 
-`,
-              }}
-            ></style>
-          }
-          <title>Shane Loeffler</title>
-          {this.props.styleTags}
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </html>
-    );
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,6 @@ import * as React from "react";
 import Layout from "../components/Layout";
 import Highlight from "../components/Highlight";
 
-if (process.browser) {
-  document.body.classList.add("loaded");
-}
 const projects = [
   {
     img: "../static/img/maptheblacksnake.png",
